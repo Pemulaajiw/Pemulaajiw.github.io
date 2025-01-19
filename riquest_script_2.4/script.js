71e4cd8c8d84ab1fad78b78e3a1ff389c54b5895
PROJECTS TEAM 🅥🅝: FANVPN Copyright 2024 <163427716+Pemulaajiw@users.noreply.github.com>
2025-01-19 10:33:24 +0700

    Update script.js

--- riquest_script_2.4/script.js
+++ riquest_script_2.4/script.js
@@ -5,7 +5,7 @@ function sendWhatsAppRequest() {
     // Build WhatsApp message with form data
     var message = "BELI%20SCRIPT%20FANSCTUNNEL%20V2%0A";
     formData.forEach(function(value, key) {
-        message += key + "%3A%20" + value + "%0A";
+        message += key + "%2A%20" + value + "%0A";
     });
 
     // Construct the WhatsApp URL with the message
