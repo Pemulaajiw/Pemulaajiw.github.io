f09f00d49642d9569fc8d5cc268a1b218f1ede1b
PROJECTS TEAM 🅥🅝: FANVPN Copyright 2024 <163427716+Pemulaajiw@users.noreply.github.com>
2025-10-11 19:28:10 +0700

    Update script.js

--- riquest_sg_do/script.js
+++ riquest_sg_do/script.js
@@ -3,14 +3,14 @@ function sendWhatsAppRequest() {
     var formData = new FormData(form);
 
     // Build WhatsApp message with form data
-    var message = "REQUEST%20AKUN%20ANDA%0A▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬%0ASERVER%20SG%20VIP%0A";
+    var message = "REQUEST%20AKUN%20ANDA%0A▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬%0ASERVER%20SG%20DO%20SPEED2&3%0A";
     var message1 = "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬%0A";
     formData.forEach(function(value, key) {
         message += key + "%3A%20" + value + "%0A";
     });
 
     // Construct the WhatsApp URL with the message
-    var whatsappUrl = "https://wa.me/6285238906544?text=" + message + message1;
+    var whatsappUrl = "https://wa.me/6287898083051?text=" + message + message1;
 
     // Redirect user to WhatsApp
     window.location.href = whatsappUrl;
