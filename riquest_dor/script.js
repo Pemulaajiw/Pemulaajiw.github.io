bc8233ec3e1e4b96a99bc13b67e36eeb6e43c91b
PROJECTS TEAM 🅥🅝: FANVPN Copyright 2024 <163427716+Pemulaajiw@users.noreply.github.com>
2025-10-18 14:05:00 +0700

    Create script.js

---
+++ riquest_dor/script.js
@@ -0,0 +1,16 @@
+function sendWhatsAppRequest() {
+    var form = document.getElementById('requestForm');
+    var formData = new FormData(form);
+
+    // Build WhatsApp message with form data
+    var message = "BELI%20PAKET%20XL%202%20BULANAN%0A";
+    formData.forEach(function(value, key) {
+        message += key + "%3A%20" + value + "%0A";
+    });
+
+    // Construct the WhatsApp URL with the message
+    var whatsappUrl = "https://wa.me/6287898083051?text=" + message ;
+
+    // Redirect user to WhatsApp
+    window.location.href = whatsappUrl;
+}
