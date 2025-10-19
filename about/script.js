1de59ce2217eff57c9120b333ae9d93ae734990e
PROJECTS TEAM 🅥🅝: FANVPN Copyright 2024 <163427716+Pemulaajiw@users.noreply.github.com>
2025-10-19 14:30:18 +0700

    Update script.js

--- about/script.js
+++ about/script.js
@@ -9,4 +9,14 @@ window.onscroll = function() {
         header.style.top = "-100px";
     }
     prevScrollpos = currentScrollPos;
+
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom > 0) {
+            section.classList.add('show');
+        } else {
+            section.classList.remove('show');
+        }
+    });
 }
